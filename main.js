32998dc3793d9c94e4546ef82ecbd0198dcfb7bf
vitoriadamasceno1 <159484584+vitoriadamasceno1@users.noreply.github.com>
2024-05-09 17:40:45 -0300

    09/05

--- main.js
+++ main.js
@@ -16,9 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
